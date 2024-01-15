66d4e6ec67652c5f43d03a6d6aaa9164df0b149d
Usama-Abuelnour <usama.abuelnour@gmail.com>
2024-01-15 17:03:24 +0200

    update1

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,20 @@
 import classes from "./Layout.module.scss";
 import logo from "../assets/logo.svg";
 import Modal from "../UI/Modal/Modal";
+import { useEffect, useRef, useState } from "react";
 const Layout = ({ children, score, modalIsVisible, openModal, closeModal }) => {
+    const [height, setHeight] = useState(innerHeight);
+    const mount = useRef(true);
+    useEffect(()=>{
+        if(mount.current) mount.current = false;
+        else {setHeight(window.innerHeight)}
+        // setHeight(window.innerHeight)
+        // console.log('changed')
+     }, [height])
+
+     window.onresize = () => setHeight(innerHeight);
     return (
-        <div className={classes.container}>
+        <div className={classes.container} style={{height: `${height}px` }}>
             <Modal modalIsVisible={modalIsVisible} closeModal={closeModal}  />
             <header className={classes.header}>
                 <div className={classes.logo}>
